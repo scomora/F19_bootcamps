59d252698c13f450b208d47b18a9b4125ba38e2f
scomora <33465154+scomora@users.noreply.github.com>
2019-09-18 00:09:20 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-//This file holds any configuration variables we may need 
-//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
-//copy this file's contents to another file 'config.js' and store your MongoLab uri there
-
-module.exports = {
-    db: {
-      uri: 'mongodb+srv://bootcamp-user:cen3031@cen3031-course-lofjs.mongodb.net/test?retryWrites=true&w=majority'
-    }
-  };
